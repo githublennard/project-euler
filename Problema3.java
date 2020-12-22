fbe065f8cf8eff04e5af20a1f12318afe794573a
githublennard <lennardwilde@gmail.com>
2020-12-23 00:26:36 +0100

    Workin on Problema3

--- Problema3.java
+++ Problema3.java
@@ -1,2 +1,25 @@
 public class Problema3 {
+/*
+        The prime factors of 13195 are 5, 7, 13 and 29.
+        What is the largest prime factor of the number 600851475143 ?
+*/
+    public static void main(String[] args) {
+        //long limit = 600851475143L;
+        int limit = 13195;
+        int aux = 0;
+        int div =1;
+        while(div < limit){
+            if(limit % div == 0){
+                aux = div;
+            }
+            div ++;
+        }
+        /*for(int cont=1; cont < limit ; cont++  );{
+            //div += 1;
+            if(limit % cont == 0){
+                aux =div;
+                 }
+            }*/
+        System.out.println(aux);
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ProjectEuler
 ### Are exercises to develop programming skills
+
+````
+Local workplaces it is on my laptop.
+Git Repository is sincronized in: /home/lennard/IdeaProjects/ProjectEuler/src 
+I'm using Java 11.
+````
