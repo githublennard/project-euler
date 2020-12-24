eae9b712ab1c92f9afbfa878612f5cb7d5cc24d6
githublennard <lennardwilde@gmail.com>
2020-12-24 17:26:09 +0100

    Solved Problema3

--- Problema3.java
+++ Problema3.java
@@ -1,25 +1,37 @@
+import java.util.ArrayList; // import the ArrayList class
+
 public class Problema3 {
-/*
-        The prime factors of 13195 are 5, 7, 13 and 29.
-        What is the largest prime factor of the number 600851475143 ?
-*/
+    /*
+           The prime factors of 13195 are 5, 7, 13 and 29.
+           What is the largest prime factor of the number 600851475143 ?
+   */
     public static void main(String[] args) {
-        //long limit = 600851475143L;
-        int limit = 13195;
-        int aux = 0;
-        int div =1;
-        while(div < limit){
-            if(limit % div == 0){
-                aux = div;
+        ArrayList<Integer> priFac = new ArrayList<Integer>();
+        ArrayList<Long> cocienteDiv = new ArrayList<Long>();
+        long limit = 600851475143L;//Numero Obligatorio del ejercicio
+        //int limit = 13195;
+        //int limit = 131;//3/7/11/Numeros primos
+        int aux1 = 0;
+        long aux2 = 0;//Tiene que ser Long para que sea generico//valor de los cocientes
+        int div = 2;//Descarto el 1 por los numeros primos
+        while (div <= limit) {
+            if (limit % div == 0) {
+                aux1 = div;
+                System.out.println(aux1);//Me imprime un factor primo del numero
+                priFac.add(div);//Si se divide un numero por su primer factor primo se obtiene el fp mas grande
+                aux2 = (limit / div);//Cociente de la division de un numero entre su primer factor primo
+                cocienteDiv.add(aux2);
+                if (priFac.size() > 3) {
+                    break;
+                }
             }
-            div ++;
+            div++;
         }
-        /*for(int cont=1; cont < limit ; cont++  );{
-            //div += 1;
-            if(limit % cont == 0){
-                aux =div;
-                 }
-            }*/
-        System.out.println(aux);
+        if (priFac.size() >= 2) {
+            System.out.println("El numero: " + limit + " No es Primo");
+            System.out.println("El factor primo mas grande es: " + cocienteDiv.get(0));//Me imprime factor primo mas grande
+        } else {
+            System.out.println("El numero: " + limit + " es Primo");
+        }
     }
 }
