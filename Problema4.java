79ed63e57a85fccdc0b415b2ee30985f1c527446
githublennard <lennardwilde@gmail.com>
2020-12-31 01:58:40 +0100

    Solution without comments

--- Problema4.java
+++ Problema4.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Problema4 {
-    public static ArrayList<Integer> palindro;//Array available to all the methods
+    public static ArrayList<Integer> palindro;
 
     /*
         A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
@@ -10,14 +10,13 @@ public class Problema4 {
     */
     //**This Code find the largest palindromic number.
     static void checkNumbers(int num1, int num2) {
-        System.out.println("Numero que inician el bucle en el metodo checkNumbers: " + num1 + "  " + num2);
+        System.out.println("Numeros que inician en el metodo checkNumbers: " + num1 + "  " + num2);
         for (int aux = num1; aux >= 100; aux--) {
             int multi = aux * num2;
-            String number = "" + multi;//Casting 'int' to 'string'
+            String number = "" + multi;
             char[] array = number.toCharArray();
             if (array.length == 6 && array[5] == array[0] && array[4] == array[1] && array[3] == array[2]) {
                 System.out.println("El Numero Palindromico es: " + multi);
-                System.out.println("Producto de: " + aux + " y " + num2);
                 palindro.add(multi);
             }
         }
@@ -27,7 +26,6 @@ public class Problema4 {
         palindro = new ArrayList<Integer>();
         int num1 = 999;
         int num2 = 999;
-//        while (num1 >= 100 && num2 >= 100 && num2 > 800 && num1 > 800) {//Another way to stop the Bucle 'while'
         while (num1 >= 100 && num2 >= 100 && palindro.size() < 100) {
             checkNumbers(num1, num2);
             num1--;
@@ -36,6 +34,5 @@ public class Problema4 {
         }
         int maximo = Collections.max(palindro);
         System.out.println("Solucion del Ejercicio: " + maximo);
-        System.out.println(palindro.size());
     }
 }
